f5119c1a17378b84d56619e0347a261511e2fa42
simone <simone.peruzzo@bizeta.net>
2024-08-16 15:03:28 +0200

    LP1.1 commit

---
+++ LP.1.1-IntroductionToDevOps/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace LP._1._1_IntroductionToDevOps.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
