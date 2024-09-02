9e32b04695cd4972e6c0411a8a13d9ca635d96fc
simone <simone.peruzzo@bizeta.net>
2024-09-02 14:41:00 +0200

    Feature 1 added

---
+++ LP1.3-DesignAndImplementBranchStrategiesAndWorkflows/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace LP1._3_DesignAndImplementBranchStrategiesAndWorkflows.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
